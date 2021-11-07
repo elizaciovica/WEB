a68521259ea82c260049fe1ef97c1540b15a5de1
elizaciovica <elizaciovica0208@gmail.com>
2021-11-07 20:13:26 +0200

    offer page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {
+    :sign_in => "login",
+    :sign_out => "logout",
+    :sign_up => "register"
+  }
   get 'contact' => 'pages#contact'
   resources :offers
   root 'offers#index'
