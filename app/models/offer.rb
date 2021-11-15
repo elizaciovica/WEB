091b2a9e3b098e0156ad95d58460a64fb59e59d1
elizaciovica <elizaciovica0208@gmail.com>
2021-11-15 19:15:42 +0200

    form validation and style improve

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,20 @@
 class Offer < ApplicationRecord
-    has_one_attached :image
+    has_one_attached :image, :dependent => :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :phone, presence: true
+  validates :location, presence: true
+  validates :meals, presence: true
+  validates :host, presence: true
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
+  validates :stars, presence: true, :inclusion => 1..5
+  validates :cleanliness, presence: true, :inclusion => 0..100
+  validates :food, presence: true, :inclusion => 0..100
+  validates :atmosphere, presence: true, :inclusion => 0..100
+
+  def get_rating(offer)
+    (offer.cleanliness + offer.food + offer.atmosphere) / 3
+  end
+
 end
